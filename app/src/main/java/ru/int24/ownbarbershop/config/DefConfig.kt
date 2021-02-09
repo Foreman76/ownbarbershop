51b3bb691052f0b72e0b4fda3b4488a63a57e83a
Macbook <alex@integra24.biz>
2021-02-09 15:58:41 +0700

    1

--- app/src/main/java/ru/int24/ownbarbershop/config/DefConfig.kt
+++ app/src/main/java/ru/int24/ownbarbershop/config/DefConfig.kt
@@ -1,8 +1,8 @@
 package ru.int24.ownbarbershop.config
 
 object DefConfig {
-    const val id: Int = 391840
-    const val api: String = "juz7br87734hzu7mbr6s"
+    const val id: Int = 0
+    const val api: String = "j"
     val user_token: String = ""
 }
 
